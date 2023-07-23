828e23a2cbe832d0aa6e6edc256a6e1c93140688
teklumezgebo <mezge002@umn.edu>
2023-07-23 17:10:17 -0500

    routes established

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   
-  resources :destinations
+  resources :destinations, only: [:index, :show, :create] 
   resources :reviews
-  resources :users
+  resources :users, only: [:index, :show, :create]
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
