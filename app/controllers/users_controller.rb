93d591378c61a2d9a0833292ad1e3b4c241a7608
teklumezgebo <mezge002@umn.edu>
2023-07-23 18:24:39 -0500

    private methods for user controller created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,5 +14,19 @@ class UsersController < ApplicationController
         render json: user, status: :created
     end
 
+    private
+
+    def user_params
+        params.permit(:username)
+    end
+
+    def user_not_found
+        render json: { error: "User not found" }, status: :not_found 
+    end
+
+    def invalid_user(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
 
 end
