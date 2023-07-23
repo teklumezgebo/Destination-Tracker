7e2f17d4f5ed2a5f34ade0535864bc399d3e43d5
teklumezgebo <mezge002@umn.edu>
2023-07-23 18:22:23 -0500

    user controller routes created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+
+    def index
+        render json: User.all, status: :ok
+    end
+
+    def show
+        user = User.find(params[:id])
+        render json: user, status: :ok
+    end
+
+    def create
+        user = User.create!(user_params)
+        render json: user, status: :created
+    end
+
+
 end
