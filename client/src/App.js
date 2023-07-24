0ffe4ce766da4f72b42bf79ad16e35dca93ff96e
teklumezgebo <mezge002@umn.edu>
2023-07-24 17:17:51 -0500

    nav bar component created

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,19 @@
 // import { useState, useEffect } from 'react'
 import Destination from './Destination';
+import NavBar from './NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <NavBar />
       <h1>Destination Tracker</h1>
       <h2>Login/Signup</h2>
       <br></br>
       <h1>List of destinations</h1>
       <Destination />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ client/src/NavBar.js
@@ -0,0 +1,17 @@
+function NavBar() {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                    <a className="nav-item nav-link" href="#">Home</a>
+                    <a className="nav-item nav-link" href="#">Destinations</a>
+                    <a className="nav-item nav-link" href="#">Reviews</a>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar
