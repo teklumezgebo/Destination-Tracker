b02635c27e0ed44648eca58073bedc5f74d8d0db
teklumezgebo <mezge002@umn.edu>
2023-08-06 17:07:52 -0500

    sign in and login routes created

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
 class SessionsController < ApplicationController
+
+    def create
+        user = User.find(username: params[:username])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id 
+            render json: user, status: :created
+        else
+            render json: { error: {login: "Invalid username or password"} } 
+        end
+    end
+
+    def destroy
+        session.delete :user_id
+        head :no_content
+    end
+
 end
