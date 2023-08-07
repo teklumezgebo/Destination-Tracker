42305523a680d8b465712d022cb9bdc78eb961c7
teklumezgebo <mezge002@umn.edu>
2023-08-07 13:58:53 -0500

    user title function created

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -1,11 +1,24 @@
 function Profile({ user }) {
+    
+    function setUserTitle() {
+        if (user.destinations.length === 0) {
+            return <p>New Traveler</p>
+        } else if (user.destinations.length > 0 && user.destinations.length <= 5) {
+            return <p>Novice Traveler</p>
+        } else if (user.destinations.length > 6 && user.destinations.length <= 15) {
+            return <p>Experienced Traveler</p>
+        } else if (user.destinations.length > 16) {
+            return <p>Expert Traveler</p>
+        }   
+    }
+
     return(
         <div>
             <h3 className="text-left pt-5">{user.username}</h3>
-            <p>Title based on how many destinations</p>
+            {setUserTitle()}
             <h1 className="pt-5 pr-1">Places you've been</h1>
             <div>
-                {/* {list of destination cards} */}
+                {user.destinations}
             </div>
         </div>
     )
