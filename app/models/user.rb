a663b358fde102e244390b56203fbcb429356011
teklumezgebo <mezge002@umn.edu>
2023-08-23 13:10:58 -0500

    render-build.sh updated script

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
+    has_secure_password
     has_many :reviews
     has_many :destinations, through: :reviews
-    has_secure_password
 
     validates :username, presence: true
     validates :username, uniqueness: true
